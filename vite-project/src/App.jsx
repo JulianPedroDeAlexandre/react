a558907781111ed8516960d67309939b8be43533
Julian Pedro de alexandre <julian.zep@gmail.com>
2025-11-01 15:23:30 -0300

    upando react

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,31 @@
+export default function App (){
+  return(
+    <div>
+      <img src="/react.png" alt="" />
+      <h1>React</h1>
+      <p>A biblioteca para inerfaces de usuario web e nativa</p>
+      <button>Aprenda React</button>
+      <button>Referencia do React</button>
+      <hr />
+      <h2>Crie interfaces de usuario de componentes</h2>
+      <p>
+        react permite que voce consrua interfaces de usuarios a partir de pedaços individuais chamados componentes.
+      </p>
+      <hr />
+      <h2>Ecreva componentes com codifo e marcação</h2>
+      <p>Componenstes React são funções JavaScript. A sintaxe de marcação é chamada JSX. É uma extensão da sintaxe Java Script popularizada pelo React.</p>
+      <hr />
+      <h2>Proximos passos</h2>
+      <ul>
+        <li>
+          uso de daddos dinamicos no JSX
+        </li>
+        <li>Criação de novos componentes</li>
+        <li>Estiliação de componentes</li>
+        <li>Reutilização de componentes</li>
+        <li>Uso de props e children</li>
+        <li>Uso de eventos Java Script</li>
+      </ul>
+    </div>
+  )
+}
